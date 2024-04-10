b61e8ad44a9e50dc8f483e6c990a6af7d0db071d
Noah Steckley <Noah3141@gmail.com>
2024-04-09 17:18:20 -0700

    Getting more comfortable with bash scripting and raw SQL

---
+++ take_input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Greet user and request their name
+echo "Enter your name"
+read -p "Name: " name
+
+activity[0] = "Football"
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /c/Users/Noah3/Code/bash-mysql-todo-app/
+
+pwd
+cd test
+pwd
